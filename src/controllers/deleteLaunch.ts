fbb1da76ed84c0f1e0b8aef0bd724e11d6ac6a11
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-01-13 16:40:15 +0200

    delete launch done

--- src/controllers/deleteLaunch.ts
+++ src/controllers/deleteLaunch.ts
@@ -1,5 +1,20 @@
 import { RequestHandler } from "express";
+import Launch from "../models/launches";
 
-export const deleteLaunche:RequestHandler = (req,res,next) => {
-  
+interface ReqBody {
+	id: number;
 }
+
+export const deleteLaunche: RequestHandler<any, any, ReqBody> = async (
+	req,
+	res,
+	next
+) => {
+	const { id } = req.body;
+
+	if (await Launch.delete(id)) {
+		res.status(200).json({ msg: "launch aborted" });
+		return;
+	}
+	res.status(400).json({ msg: "there is no launch with that id" });
+};
