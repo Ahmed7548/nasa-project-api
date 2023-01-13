4c2919263c3b86b5211899f206e827d4d8da34a2
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-01-13 15:20:21 +0200

    finishing up with get launches functionality

--- src/models/launches.ts
+++ src/models/launches.ts
@@ -1,11 +1,50 @@
 import { openDb } from "../db/db";
 
-class Launch {
-  constructor(private date:Date,private name:string, private rocketType: string, private destination:number) {
-  }
-  
-
-  async save() {
-    
-  }
+interface DbLaunch {
+	ID: number;
+	date: string;
+	name: string;
+	rocketType: string;
+	destination: number;
 }
+
+class Launch {
+	constructor(
+		private date: Date,
+		private name: string,
+		private rocketType: string,
+		private destination: number
+	) {}
+
+	async save(): Promise<boolean> {
+		const db = await openDb();
+		const result = await db.run(
+			`INSERT INTO Launches (date, name, rocketType, destination) VALUES (?, ?, ?, ?)`,
+			this.date.toLocaleDateString(),
+			this.name,
+			this.rocketType,
+			this.destination
+		);
+		await db.close();
+		if (result.changes) {
+			return true;
+		}
+		return false;
+	}
+	static async delete(id: number): Promise<boolean> {
+		const db = await openDb();
+		const result = await db.run(`DELETE FROM Launches WHERE ID=?`, id);
+		await db.close();
+		if (result.changes) {
+			return true;
+		}
+		return false;
+	}
+	static async getAll(): Promise<DbLaunch[]> {
+		const db = await openDb();
+		const results = await db.all<DbLaunch[]>(`select * from Launches`);
+		return results;
+	}
+}
+
+export default Launch;
