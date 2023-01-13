e112b84cccd553b396f58f1808c655dbb4515fc7
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-01-13 13:27:40 +0200

    launches router structure

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,24 @@
-import * as dotenv from 'dotenv'
-import cors from "cors"
-import express from "express"
-import populatedb from './db/populatedb'
-import planetRouter from './routes/plantets'
+import * as dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import populatedb from "./db/populatedb";
+import planetRouter from "./routes/plantets";
 
-dotenv.config()
+dotenv.config();
 
-
-
-
-const app = express()
-
+const app = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 // routes
-app.use("/planets",planetRouter)
-
-
-
-
+app.use("/planets", planetRouter);
 
 //error handlers
 
-
-
-
 // app start
 app.listen(process.env.PORT, () => {
-  populatedb()
-  console.log("app is running on port "+process.env.PORT)
-})
+	populatedb();
+	console.log("app is running on port " + process.env.PORT);
+});
---
+++ src/routes/launches.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+
+const launchesRouter = Router()
+
+
+
+launchesRouter.get("/",)
+launchesRouter.post("/",)
+launchesRouter.delete("/",)
+
+
+
+
+
+
+export default launchesRouter
