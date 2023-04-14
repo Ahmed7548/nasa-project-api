6277c2250d86290282d391172d966b7bee7ff16f
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-04-14 03:24:13 +0200

    refactoring the error handling of the routes by by using higher order function catchAsyncError to prevent repetition

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,5 +8,7 @@ import { RequestHandler } from "express";
 export type ResType<T> = T | ErrorResponse
 
 
-export type  CustomRequestHandler<ResBody=any,ReqBody=any,ReqQuery=any>=RequestHandler<any,ResType<ResBody>,ReqBody,ReqQuery>
+export type CustomRequestHandler<ResBody = any, ReqBody = any, ReqQuery = any> = RequestHandler<any, ResType<ResBody>, ReqBody, ReqQuery>
+
+export type Promisfy<F extends (...args:any[])=>any>=(...args:Parameters<F>)=>Promise<Awaited<ReturnType<F>>> 
 
